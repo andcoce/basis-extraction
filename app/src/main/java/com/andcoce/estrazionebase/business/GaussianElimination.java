bbb348386ed46d2af1e85d99e4cb81c9c69c62d9
andcoce <andcocever@gmail.com>
2023-10-22 22:07:52 +0200

    Echelon Algorithm Working!

--- app/src/main/java/com/andcoce/estrazionebase/business/GaussianElimination.java
+++ app/src/main/java/com/andcoce/estrazionebase/business/GaussianElimination.java
@@ -1,29 +1,66 @@
 package com.andcoce.estrazionebase.business;
 
-import com.andcoce.estrazionebase.bean.Matrix;
+import android.icu.text.DecimalFormat;
+import android.util.Log;
 
-import java.util.ArrayList;
+import com.andcoce.estrazionebase.bean.Matrix;
 
 public class GaussianElimination {
 
     private Matrix matrix;
 
+    private DecimalFormat decfor;
+
     public GaussianElimination(Matrix matrix){
         this.matrix = matrix;
+        decfor = new DecimalFormat("0.00");
     }
 
     public void echelonForm(){
         for(int col = 0; col < matrix.getN_VECTORS(); col++){
-            checkFirstElement(col);
+            if(col != 0){
+                System.out.println();
+            }
+            rearrange(col); //metodo che riordina le righe in base agli zeri prima di ogni operazione
+            rowOperations(col);
         }
     }
 
-    private void checkFirstElement(int col){
+    private void rearrange(int col){
         for(int row = 0; row < matrix.getVECTOR_DIM(); row++){
             if(matrix.getMatrix()[col][row] == 0 && previousAreZero(row, col)){
                 swapRows(row, findRow(row, col));
             }
         }
+    }
+
+    private void rowOperations(int col){
+        int pivotRow = findPreviousPivot(col);
+        for(int row = pivotRow + 1; row < matrix.getVECTOR_DIM(); row++){ //lascio invariata la riga utilizzata per le operazioni
+            if(matrix.getMatrix()[col][row] != 0) {
+                double a = matrix.getMatrix()[col][pivotRow];
+                double b = matrix.getMatrix()[col][row];
+                double div = b/a;
+                addFirstRowComb(row, col, div, pivotRow);
+            }
+        }
+    }
+
+    private void addFirstRowComb(int target, int start, double div, int pivotRow){
+        for(int i = start; i < matrix.getN_VECTORS(); i++){
+            double r = matrix.getMatrix()[i][target] - div*matrix.getMatrix()[i][pivotRow];
+            matrix.getMatrix()[i][target] = Double.parseDouble(decfor.format(r));
+        }
+    }
+
+    private int findPreviousPivot(int col){
+        for(int i = 0; i < matrix.getVECTOR_DIM(); i++){
+            if(previousAreZero(i, col) && matrix.getMatrix()[i][col] != 0){
+                return i;
+            }
+        }
+
+        return matrix.getVECTOR_DIM();
     }
 
     private int findRow(int start, int col){
@@ -48,7 +85,7 @@ public class GaussianElimination {
 
     private boolean previousAreZero(int row, int col){
         boolean prev = true;
-        for(int i = col; i >= 0; i--){
+        for(int i = col - 1; i >= 0; i--){
             if(matrix.getMatrix()[i][row] != 0){
                 prev = false;
             }
